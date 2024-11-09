953fb26ae97b94b5af3d7e267a5c4bbb2a86667e
Muhammad Sarim Mehboob <165645321+Sarim4545@users.noreply.github.com>
2024-11-09 18:16:22 +0500

    script.ts

--- script.ts
+++ script.ts
@@ -4,10 +4,10 @@ const skillsContent = document.getElementById('skills-content') as HTMLElement;
 togglebutton.addEventListener('click', () => {
     // Check if the content is already displayed using 'display' property
     if (skillsContent.style.display === 'none' || skillsContent.style.display === '') {
-        skillsContent.style.display = 'block';  // Show content by setting display to block
-        skillsContent.classList.add('show');    // Also add 'show' class for consistency (optional)
+        skillsContent.style.display = 'block';  
+        skillsContent.classList.add('show');    
     } else {
-        skillsContent.style.display = 'none';   // Hide content by setting display to none
-        skillsContent.classList.remove('show'); // Remove 'show' class
+        skillsContent.style.display = 'none';   
+        skillsContent.classList.remove('show'); 
     }
 });
